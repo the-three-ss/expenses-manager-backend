83ff788a0289f63b687d4365b52705e71d630407
pshital <skpawar@gmail.com>
2019-06-15 20:41:02 +0100

    update after adding get, post in expenses.js

--- expenses.js
+++ expenses.js
@@ -10,22 +10,36 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: ""
+  database: process.env.DB_NAME
 });
-app.get("/", function (request, response) {
+app.get("/expenses", function (request, response) {
+  connection.query("SELECT * FROM expenses", function (err, result, fields) {
+    if (err) {
+      console.log("Error fetching expenses", err);
+      response.status(500).json({
+        error: err
+      });
+    } else {
+      response.json({ expenses: result });
+    }
   });
-
-  app.post("/", function (request, response) {
-    
+ });
+ 
+ app.post("/expenses", function (request, response) {
+  const expensesToBeSaved = request.body;
+  connection.query('INSERT INTO expenses SET ?', [expensesToBeSaved], function (err, results, fields) {
+    if (err) {
+      console.log("Error fetching expenses", err);
+      response.status(500).json({
+        error: err
+      });
+    } else {
+      response.json({
+        expenses_id: results.insertId
+      });
+    }
   });
+ });
 
-  app.delete("/", function(request, response) {
-    const taskId = request.params.id;
-    
-  });
-
-  app.put("/", function(request, response) {
-    
-  });
-
+  
   module.exports.handler = serverless(app);
